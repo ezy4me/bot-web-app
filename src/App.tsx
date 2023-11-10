87c95f769f644a61bcc0dac786e173cc74cafa1f
ezy4me <mrronal@mail.ru>
2023-11-10 22:49:17 +0300

    fix scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Form from "./components/Form";
 import Header from "./components/Header";
 import ProductInfo from "./components/ProductInfo";
 import ProductsList from "./components/ProductsList";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/store";
 import ScrollToTop from "./components/ScrollToTop";
