06840ac8b6b4929cedc7f1a63d1f063a96733b59
ezy4me <mrronal@mail.ru>
2023-11-10 22:47:11 +0300

    fix scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import Form from "./components/Form";
 import Header from "./components/Header";
 import ProductInfo from "./components/ProductInfo";
 import ProductsList from "./components/ProductsList";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/store";
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 const store = setupStore();
 
 function App() {
@@ -13,12 +13,14 @@ function App() {
     <Provider store={store}>
       <div className="app">
         <Header />
-        <ScrollToTop/>
-        <Routes>
-          <Route index element={<ProductsList/>}/>
-          <Route path="/form" element={<Form/>}/>
-          <Route path="/product" element={<ProductInfo/>}/>
-        </Routes>
+        <Router>
+          <ScrollToTop />
+          <Routes>
+            <Route index element={<ProductsList />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/product" element={<ProductInfo />} />
+          </Routes>
+        </Router>
       </div>
     </Provider>
   );
