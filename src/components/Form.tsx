1d7d8e8283590058945b645edbf844159f75dc8a
ezy4me <mrronal@mail.ru>
2023-11-10 21:55:50 +0300

    add date-picker

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Input from "../UI/Input";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Form = () => {
   const [name, setName] = React.useState("");
   const [surname, setSurname] = React.useState("");
   const [patronymic, setPatronymic] = React.useState("");
+
+  const [startDate, setStartDate] = React.useState(null);
+  const [endDate, setEndDate] = React.useState(null);
 
   const onChangeName = (newValue: string) => {
     setName(newValue);
@@ -20,6 +25,21 @@ const Form = () => {
   return (
     <div className="form">
       <h3>Введите данные для аренды</h3>
+      <div className="form__dates">
+        <DatePicker
+          placeholderText="Дата получения"
+          dateFormat="dd / MM / yyyy"
+          selected={startDate}
+          onChange={(date: any) => setStartDate(date)}
+        />
+        <DatePicker
+          placeholderText="Дата сдачи"
+          dateFormat="dd / MM / yyyy"
+          selected={endDate}
+          onChange={(date: any) => setEndDate(date)}
+        />
+      </div>
+
       <Input placeHolder="Имя" value={name} onChange={onChangeName} />
       <Input placeHolder="Фамилия" value={surname} onChange={onChangeSurname} />
       <Input
