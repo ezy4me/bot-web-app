611c056e394360c6c6f2b409033fc38044bffb89
ezy4me <mrronal@mail.ru>
2023-11-10 13:54:12 +0300

    init

--- src/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./button.css";
-
-const Button = (props: any) => {
-  return <button {...props} className={"button " + props.className} />;
-};
-export default Button;
