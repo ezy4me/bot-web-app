131c3be4de3fe61b4a47a1094d73fcc60a71b195
ezy4me <mrronal@mail.ru>
2023-11-10 14:09:31 +0300

    add close button

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+// interface HeaderProps {
+//   title: string;
+// }
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='header-inner'>
+        <input type="text" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
